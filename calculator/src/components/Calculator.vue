0c6cc691526c07badd8d5b124fe6b80494626cf2
Lukas <149614931+LukasKaeb@users.noreply.github.com>
2024-08-01 23:22:59 +0200

    Implemented calculator logic

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -3,47 +3,88 @@
 <div class="main-container">
     <h1>Calculator</h1>
     <div class="output">
-        <p>Output</p>
+        <p>{{ displayOutput }}</p>
     </div>
 
     <div class="operations">
-        <button class="operation">AC</button>
-        <button class="operation">DEL</button>
-        <button class="operation">/</button>
-        <button class="operation">-</button>
+        <button @click="clearDisplay" class="operation">AC</button>
+        <button @click="deleteDigit()" class="operation">DEL</button>
+        <button @click="appendToDisplay('/')" class="operation">/</button>
+        <button @click="appendToDisplay('-')" class="operation">-</button>
     </div>
 
     <div class="numpad-row-1">
-        <button class="numpad-button">7</button>
-        <button class="numpad-button">8</button>
-        <button class="numpad-button">9</button>
-        <button class="operation">*</button>
+        <button @click="appendToDisplay('7')" class="numpad-button">7</button>
+        <button @click="appendToDisplay('8')" class="numpad-button">8</button>
+        <button @click="appendToDisplay('9')" class="numpad-button">9</button>
+        <button @click="appendToDisplay('*')" class="operation">*</button>
     </div>
 
     <div class="numpad-row-2">
-        <button class="numpad-button">4</button>
-        <button class="numpad-button">5</button>
-        <button class="numpad-button">6</button>
-        <button class="operation">+</button>
+        <button @click="appendToDisplay('4')" class="numpad-button">4</button>
+        <button @click="appendToDisplay('5')" class="numpad-button">5</button>
+        <button @click="appendToDisplay('6')" class="numpad-button">6</button>
+        <button @click="appendToDisplay('+')" class="operation">+</button>
 
     </div>
     
     <div class="numpad-row-3">
-        <button class="numpad-button">0</button>
-        <button class="numpad-button">1</button>
-        <button class="numpad-button">2</button>
-        <button class="numpad-button">3</button>
+        <button @click="appendToDisplay('0')" class="numpad-button">0</button>
+        <button @click="appendToDisplay('1')" class="numpad-button">1</button>
+        <button @click="appendToDisplay('2')" class="numpad-button">2</button>
+        <button @click="appendToDisplay('3')" class="numpad-button">3</button>
     </div>
+        
+        <div class="numpad-row-4">
+            <button @click="appendToDisplay('.')" class="numpad-button">.</button>
+            <button @click="calculate()" class="numpad-button">=</button>
+        </div>
 
 </div>
 
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+let displayOutput = ref('')
+
+const appendToDisplay = (input) => {
+    displayOutput.value += input
+}
+
+const clearDisplay = () => {
+    displayOutput.value = ''
+}
+
+const calculate = () => {
+
+    try{
+
+    const result = eval(displayOutput.value)
+    displayOutput.value = result
+    } catch(err){
+        displayOutput.value = 'Error'
+    }
+}
+
+const deleteDigit = () => {
+    try{
+    const deleted = displayOutput.value.slice(0, -1)
+    displayOutput.value = deleted
+    } catch{
+        displayOutput.value = 'Error'
+    }
+}
 
 </script>
 
 <style>
+
+.numpad-row-4 {
+    text-align: left;
+    margin-left: 47.5px;
+}
 h1{
     color: whitesmoke;
 }
@@ -60,7 +101,13 @@ body{
     cursor: pointer;
     border: none;
 }
+.numpad-button:hover {
+    background-color: #666;
+}
 
+.operation:hover{
+    background-color: #666;
+}
 .operation {
     height: 50px;
     width: 50px;
